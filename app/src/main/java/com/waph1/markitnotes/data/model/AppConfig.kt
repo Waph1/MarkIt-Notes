a8274a65151944a102581586fb80b46a1da9ee80
Waph1 <waph@users.noreply.github.com>
2026-02-27 16:40:51 +0100

    chore: bump version to 0.6.2

---
+++ app/src/main/java/com/waph1/markitnotes/data/model/AppConfig.kt
@@ -0,0 +1,7 @@
+package com.waph1.markitnotes.data.model
+
+data class AppConfig(
+    var fileColors: java.util.HashMap<String, Long> = java.util.HashMap(), // Filename -> Color
+    var pinnedFiles: java.util.HashSet<String> = java.util.HashSet(),       // Filenames
+    var customTimestamps: java.util.HashMap<String, Long> = java.util.HashMap() // Filename -> Timestamp
+)
